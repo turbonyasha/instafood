4f22a0801c3af95056939c1cf63c17f56cc9b5ac
Jane Doe <165895898+turbonyasha@users.noreply.github.com>
2024-11-29 23:57:55 +0400

    cors update

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -41,11 +41,9 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'foodgram.urls'
 

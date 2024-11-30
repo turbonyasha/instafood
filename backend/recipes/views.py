0cea0e7092ae3cf76f716f4dc67734dae96d5658
Jane Doe <165895898+turbonyasha@users.noreply.github.com>
2024-11-30 19:22:18 +0400

    bugfix v7 url fix

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -11,7 +11,6 @@ from rest_framework.permissions import (
     IsAuthenticated, IsAuthenticatedOrReadOnly
 )
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 import core.constants as const
 from core.filters import RecipesFilterSet
